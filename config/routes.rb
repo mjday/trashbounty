54bff07c68fcd2569d1c5897b1554c2a0540cdea
don park <donpark@pm.me>
2019-08-13 15:21:12 +0800

    updated routes with resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'collections/index'
-  get 'collections/new'
-  get 'collections/create'
-  get 'banks/index'
-  get 'banks/show'
-  get 'banks/new'
-  get 'banks/create'
-  get 'banks/edit'
-  get 'banks/update'
-  get 'banks/destroy'
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/create'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :banks
+
+  resources :collections, only: [:index, :new, :create] do
+    resources :reviews, only: [:index, :new, :create]
+  end
+
 end
