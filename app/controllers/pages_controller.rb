92e18b954da456e93519328dd5bb2ef7b2222f73
don park <donpark@pm.me>
2019-08-14 17:26:10 +0800

    removed extra method in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  # root page
   def home
     @banks = Bank.all
-  end
-
-  # help page
-  def help
-    #
   end
 end
