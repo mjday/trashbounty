9f05f568de1a785c2b867a09ce93b51a0ed31388
don park <donpark@pm.me>
2019-08-14 17:23:50 +0800

    minor changes to seeds file to take only the first 10 banks

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,7 +1,7 @@
 class Bank < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  belongs_to :user, optional: true
+  belongs_to :user
   # added optional true to allow me to test banks in the terminal
   has_many :collections
   has_many :reviews, through: :collections
