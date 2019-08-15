5842316efb6116d4aefcf86b165787475a164382
don <donpark@pm.me>
2019-08-15 13:44:27 +0800

    added necessary before_action and Devises parameter sanitizer to permit the custom params we need for the User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :phone_number, :business])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :phone_number, :business, :bitcoin_address])
+  end
 
   def after_sign_in_path_for(resource)
     users_dashboard_path
