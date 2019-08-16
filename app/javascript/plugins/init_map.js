3c921adbb79958c6368c7f491f0b7ad06d895631
don <donpark@pm.me>
2019-08-16 13:57:22 +0800

    cleaned up code

--- app/javascript/plugins/init_map.js
+++ app/javascript/plugins/init_map.js
@@ -1,30 +1,24 @@
-      function initMap(){
-
-
-        const indexMap = document.getElementById('map')
-        if (indexMap) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-          const lat = position.coords.latitude
-          const lng = position.coords.longitude;
-         const map = new google.maps.Map(indexMap, {
+function initMap() {
+  const indexMap = document.getElementById('map')
+  if (indexMap) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+    const lat = position.coords.latitude
+    const lng = position.coords.longitude;
+    const map = new google.maps.Map(indexMap, {
           center: new google.maps.LatLng(lat,lng),
           zoom: 3
         });
-    //     const marker = new google.maps.Marker({
-    //       position: ,
-    //       map: map
-    // });
-        const markers = JSON.parse(indexMap.dataset.markers);
-        markers.forEach((marker) => {
-          new google.maps.Marker({
-            position: new google.maps.LatLng(marker.lat, marker.lng),
-            map: map
-          });
 
-      })
-    });
+    const markers = JSON.parse(indexMap.dataset.markers);
+      markers.forEach((marker) => {
+
+            new google.maps.Marker({
+              position: new google.maps.LatLng(marker.lat, marker.lng),
+              map: map
+            });
+          })
+      });
+    }
   }
-}
 
-
-  export { initMap };
+export { initMap };
