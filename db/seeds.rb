3aab8396a5f8a18b73ac685b19d20f87cb9143fe
Michael Day <mjd.poindexter@gmail.com>
2019-08-18 14:36:01 +0800

    in seeds file created two sets of users, banks and collectors, banks now linked to bank users only

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,22 @@ require 'open-uri'
 require 'json'
 
 10.times do |i|
-  user = User.create!(
+  user_collector = User.create!(
     email: Faker::Internet.email,
     password: "123456",
     username: Faker::Name.first_name,
     phone_number: Faker::PhoneNumber.phone_number,
+    bitcoin_address: "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"
+  )
+end
+
+10.times do |i|
+  user_bank = User.create!(
+    email: Faker::Internet.email,
+    password: "123456",
+    username: Faker::Name.first_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    business: true,
     bitcoin_address: "1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa"
   )
 end
@@ -25,7 +36,7 @@ banks.first(10).each do |bank|
     country: bank["country"],
     materials_accepted: bank["materials_accepted"],
     products_accepted: bank["products_accepted"],
-    user: User.find(rand(User.first.id..User.count))
+    user: User.find(rand(11..20))
   )
 end
 
@@ -35,7 +46,7 @@ types = ["Bitcoin", "Cash"]
     date: Date.today + rand(1..5),
     total_kg: rand(1..50),
     payment_type: types.sample,
-    user: User.find(rand(User.first.id..User.count)),
+    # user: User.find(rand(User.first.id..User.count)),
     bank: Bank.find(rand(Bank.first.id..Bank.count))
   )
   collection.total_amount = collection.total_kg * rand(1..10)
