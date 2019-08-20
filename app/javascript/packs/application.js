c26cd841308a8dc6486cd0714cc873932ecdeb16
Michael Day <mjd.poindexter@gmail.com>
2019-08-20 10:50:46 +0800

    displayed chart on dashboard, and made links to collection show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,13 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import {initAutocomplete} from "../plugins/init_autocomplete";
 import {initMap} from "../plugins/init_map";
-import {initChart} from "../plugins/init_chart";
+import {drawChart} from "../plugins/drawChart";
 
 
 // initAutocomplete();
 // initMap();
 initMapbox();
-initChart();
+drawChart();
 
 
 
