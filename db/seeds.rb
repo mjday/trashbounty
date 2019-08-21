0364a43d67bc8d22ef730f55edbd3b7cb9e60d25
don <donpark@pm.me>
2019-08-21 14:47:32 +0800

    reverted our seed into the original

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,8 +30,6 @@ serialized_banks = File.read(filepath)
 banks = JSON.parse(serialized_banks)
 
 rates = [0.35, 0.3, 0.25, 0.2, 0.15, 0.27, 0.18, 0.22, 0.32 ]
-# materials_accepted = ["ABS", "HDPE", "HIPS", "LDPE", "LLDPE", "PA",
-#   "PC", "PE", "PET", "PP", "PS", "PVC", "WastePlastic"]
 plastic_type = [ "PET", "HDPE", "LDPE", "PP" ]
 i = 0
 banks.first(10).each do |bank|
@@ -44,65 +42,44 @@ banks.first(10).each do |bank|
     products_accepted: bank["products_accepted"],
     user: User.find(rand(11..20)),
   )
-  # create 1..4 materials (plastic_types)
-  # 4.times do
-    plastic_type.each do |p|
-      Plastic.create!(
-        name: p,
-        price_per_kg: rates.sample,
-        bank: bk
-      )
-    end
-  # i += 1
-  # end
+  plastic_type.each do |p|
+    Plastic.create!(
+      name: p,
+      price_per_kg: rates.sample,
+      bank: bk
+    )
+  end
 end
 
-# types = ["Bitcoin", "Cash"]
-# 10.times do |i|
-#   verification = Verification.create!(
-#     date: Date.today + rand(1..5),
-#     total_kg: rand(1..50),
-#     payment_type: types.sample,
-#     bank: Bank.find(rand(Bank.first.id..Bank.count))
-#   )
-#   collection = Collection.new(
-#     date: verification.date,
-#     total_kg: verification.total_kg,
-#     payment_type: verification.payment_type,
-#     user: User.find(rand(1..10)),
-#     bank: verification.bank,
-#     verification: verification
-#   )
-#   collection.total_amount = collection.total_kg * rates.sample
-#   collection.save!
-# end
+types = ["Bitcoin", "Cash"]
+10.times do |i|
+  verification = Verification.create!(
+    date: Date.today + rand(1..5),
+    total_kg: rand(1..50),
+    payment_type: types.sample,
+    bank: Bank.find(rand(Bank.first.id..Bank.count))
+  )
+  collection = Collection.new(
+    date: verification.date,
+    total_kg: verification.total_kg,
+    payment_type: verification.payment_type,
+    user: User.find(rand(1..10)),
+    bank: verification.bank,
+    verification: verification
+  )
+  collection.total_amount = collection.total_kg * rates.sample
+  collection.save!
+end
 
-# verification = Verification.create!(
-#     date: Date.today + rand(1..5),
-#     total_kg: rand(1..50),
-#     payment_type: "Cash",
-#     bank: Bank.find(6)
-#   )
-# collection = Collection.new(
-#     date: verification.date,
-#     total_kg: verification.total_kg,
-#     payment_type: verification.payment_type,
-#     user: User.last,
-#     bank: verification.bank,
-#     verification: verification
-#   )
-# collection.total_amount = collection.total_kg * rates.sample
-#   collection.save!
+ratings = [1, 2, 3, 4, 5]
+comments = ["Great", "Good", "Okay", "Poor", "Terrible", "Could have been better", "Fast and efficient process", "Slow payment", "Superb, my favourite bank"]
+10.times do |i|
+  review = Review.create!(
+    rating: ratings.sample,
+    comment: comments.sample,
+    collection: Collection.find(rand(1..10)),
+    user: User.find(rand(1..10))
+  )
+end
 
-
-# ratings = [1, 2, 3, 4, 5]
-# comments = ["Great", "Good", "Okay", "Poor", "Terrible", "Could have been better", "Fast and efficient process", "Slow payment", "Superb, my favourite bank"]
-# 10.times do |i|
-#   review = Review.create!(
-#     rating: ratings.sample,
-#     comment: comments.sample,
-#     collection: Collection.find(rand(1..10)),
-#     user: User.find(rand(1..10))
-#   )
-# end
 puts "Database ready"
